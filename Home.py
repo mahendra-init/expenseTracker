1b73fda1e0339c88be5612bc7c2689aac362d1b4
Mahendra Yadav <yankit294441@gmail.com>
2022-04-10 13:12:26 +0530

    Analysis frame updated

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import calendar
 import datetime
+from lib2to3.pgen2.token import NEWLINE
 import locale
-from tabnanny import verbose
+import time
 from tkinter import *
 from tkinter import messagebox as MessageBox
 from tkinter import ttk
@@ -9,13 +10,17 @@ from tkinter.ttk import Notebook, Style
 from turtle import Screen, bgcolor, color
 import mysql.connector as mysql
 from tkcalendar import DateEntry
-from analysis import DataAnalysis as ana
 import utilities as ut
+import schedule
 
-EX_CATEGORIES = ['Food', 'Clothing','Shopping', 'Entertainment', 'Education', 'Personal', 'Medical', 'Transportation', 'Bills']
-PAY_WAYS = ['Cash', 'UPI', 'Cheque']
+EX_CATEGORIES = ['Food', 'Clothing','Shopping', 'Entertainment', 'Education', 'Personal', 'Medical', 'Transportation', 'Bills', 'Others']
+PAY_WAYS = ['Cash', 'UPI', 'Cheque', 'Card']
 IN_CATEGORIES = ['Salary', 'Others']
+
+
 class Home():
+
+    ###  Function to add new data to database
     def AddtoDB(self, a):
         con = mysql.connect(host='localhost', user='root',
                                         password='Mahendra@$28', database='expensetracker')
@@ -26,39 +31,55 @@ class Home():
             description = self.ExDescription.get()
             expense = self.ExAmount.get()
             pay_ways = self.choosed_method.get()
-            edata = [date, category, description, expense, pay_ways]
             if(date == '' or category == '' or expense == '' or pay_ways == ''):
                 MessageBox.showinfo("Insert Status", "All Field are required except 'Description' is optional!")
             else:
                 cursor.execute("insert into expenses values('"+date+"','"+category+"','"+description+"','"+expense+"','"+pay_ways+"','"+self.username+"')")
                 cursor.execute("commit")
                 MessageBox.showinfo("Insert Status", "Inserted Successfully")
-                self.TVExpenses.insert('', 'end', values=edata)
+                self.updateView()
         elif(a == "i"):
             date = ut.dateFormat(self.Idate.get())
             source = self.choosed_source.get()
+            description = self.InDescription.get()
             income = self.EIncome.get()
-            idata = [date, source, income]
             if(date == '' or source == '' or income == ''):
                 MessageBox.showinfo("Insert Status", "All Field are required")
             else:
-                cursor.execute("insert into incomes values('"+date+"','"+source+"','"+income+"','"+self.username+"')")
+                cursor.execute("insert into incomes values('"+date+"','"+source+"','"+description+"','"+income+"','"+self.username+"')")
                 cursor.execute("commit")
                 MessageBox.showinfo("Insert Status", "Inserted Successfully")
-                self.TVIncome.insert('', 'end', values=idata)
+                self.updateView()
         con.close()
+    
+    
+    
+    
 
-    # def extractData(username):
-    #     con = mysql.connect(host='localhost', user='root',
-    #                                     password='Mahendra@$28', database='expensetracker')
-    #     cursor = con.cursor()
-    #     cursor.execute("select* from expenses where username = '"+username+"'")
-    #     data = cursor.fetchall()
-    #     cursor.execute("commit")
-    #     con.close()
-    #     for i in data:
-    #         print(list(i))
+    ###    Func to update treeviews
+    def updateView(self):
+        self.TVExpenses.delete(*self.TVExpenses.get_children())
+        self.TVIncome.delete(*self.TVIncome.get_children())
+        eFetchedData = ut.extractData(self.username, "e")
+        iFetchedData = ut.extractData(self.username, "i")
+        for i in eFetchedData:
+            self.TVExpenses.insert('', 'end', values = i)
+        for i in iFetchedData:
+            self.TVIncome.insert('', 'end', values = i)
 
+
+        self.ExListView.delete(*self.ExListView.get_children())
+        self.InListView.delete(*self.InListView.get_children())
+        date1 = ut.dateFormat(self.getDate1.get())
+        date2 = ut.dateFormat(self.getDate2.get())
+        dateList = [date1, date2]
+        eSheetData = ut.analysisData(self.username, "e", dateList)
+        iSheetData = ut.analysisData(self.username, "i", dateList)
+        for i in eSheetData:
+            self.ExListView.insert('', 'end', values = i)
+        for i in iSheetData:
+            self.InListView.insert('', 'end', values = i)
+            
 
 
     def __init__(self, username):
@@ -73,7 +94,7 @@ class Home():
 
         self.F1 = Frame(self.Tab)
         self.F2 = Frame(self.Tab)
-        self.F3 = ana.analysisFrame()
+        self.F3 = Frame(self.Tab)
 
         self.Tab.add(self.F1, text='Expense')
         self.Tab.add(self.F2, text='Income')
@@ -126,8 +147,7 @@ class Home():
         self.ExPayMethod.grid(row=5, column=1, padx=5, pady=5, sticky='w')
         # ---------------------------------
 
-        BF1Add = Button(self.F1, text='Add Expense',
-                        command=lambda: Home.AddtoDB(self, "e"))
+        BF1Add = Button(self.F1, text='Add Expense',command=lambda: Home.AddtoDB(self, "e"))
         BF1Add.grid(row=6, column=1, padx=20, pady=5,
                     sticky='w', ipadx=10, ipady=10)
 
@@ -149,21 +169,8 @@ class Home():
 
         for i in TVList:
             self.TVExpenses.heading(i, text=i.title())
-        # self.TVExpenses.grid(row=5, column=0, padx=5, pady=5,
-        #                      sticky='w', columnspan=3)
 
         # --------------------Expense End--------------
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -191,33 +198,109 @@ class Home():
         self.Icategory.grid(row=2, column=1, padx=5, pady=5, sticky='w')
 
         # --------------------------------
+        self.LDescription = Label(self.F2, text='Description', font=(None, 16))
+        self.LDescription.grid(row=3, column=0, padx=5, pady=5, sticky='w')
+
+        self.InDescription = Entry(self.F2,  font=(None, 18))
+        self.InDescription.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+
+        self.DescFormat = Label(self.F2, text='" Single & Double quotes are not allowed ! "', font=(None, 10), fg="#FF0000")
+        self.DescFormat.grid(row=3, column=2, padx=5, pady=5, sticky='w')
+
+        # --------------------------------
 
         self.LIncome = Label(self.F2, text='Income', font=(None, 18))
-        self.LIncome.grid(row=3, column=0, padx=5, pady=5, sticky='w')
+        self.LIncome.grid(row=4, column=0, padx=5, pady=5, sticky='w')
 
         self.EIncome = Entry(self.F2, font=(None, 18))
-        self.EIncome.grid(row=3, column=1, padx=5, pady=5, sticky='w')
+        self.EIncome.grid(row=4, column=1, padx=5, pady=5, sticky='w')
 
         # ---------------------------------
 
         BF2Add = Button(self.F2, text='Add Income',
                         command=lambda: Home.AddtoDB(self, "i"))
-        BF2Add.grid(row=4, column=1, padx=5, pady=5,
+        BF2Add.grid(row=5, column=1, padx=5, pady=5,
                     sticky='w', ipadx=10, ipady=10)
         
         self.Lbl = Label(self.F2, text='Recents', font=(None, 15))
-        self.Lbl.place(x = 15, y = 220)
+        self.Lbl.place(x = 15, y = 250)
         # ----------------------------------
-        TV = ['Date', 'Source', 'Income']
+        TV = ['Date', 'Source', 'Description', 'Income']
         self.TVIncome = ttk.Treeview(
             self.F2, columns=TV, show='headings', height=5)
-        self.TVIncome.place(x= 20, y= 245)
+        self.TVIncome.place(x= 20, y= 285)
+        self.TVIncome.column("Date", minwidth=0, width=80, stretch=NO, anchor=CENTER)
+        self.TVIncome.column("Source", minwidth=0, width=120, stretch=NO)
+        self.TVIncome.column("Description", minwidth=0, width=250)
+        self.TVIncome.column("Income", minwidth=0, width=120, stretch=NO, anchor=CENTER)
         for j in TV:
             self.TVIncome.heading(j, text=j.title())
-        # self.TVIncome.grid(row=5, column=0, padx=5, pady=5,
-        #                    sticky='w', columnspan=3)
+
+         # --------------------Income End--------------
+
+
+
+
+        # --------------------Analysis-------------------
+
+        LDate = Label(self.F3, text='Choose Date: ', font=(None, 16))
+        LDate.place(x = 10, y = 10)
+
+        self.getDate1 = DateEntry(self.F3)
+        self.getDate1.place(x = 180, y = 15)
+
+        self.getDate2 = DateEntry(self.F3)
+        self.getDate2.place(x = 360, y = 15)
+
+        Lb = Label(self.F3, text='to', font=(None, 15))
+        Lb.place(x = 300, y = 10)
+
+
+        self.Lbl = Label(self.F3, text='Expense Sheet:', font=(None, 15))
+        self.Lbl.place(x = 5, y = 70)
+
+        self.BF1Add = Button(self.F3, text='Track',command=lambda: self.updateView())
+        self.BF1Add.place(x = 300, y = 50)
+
+
+        ExList = ['Date', 'Category', 'Description', 'Amount Spent', 'Paid Through']
+        self.ExListView = ttk.Treeview(
+            self.F3, columns=ExList, show='headings', height=9)
+        self.ExListView.place(x= 10, y= 105)
+        self.ExListView.column("Date", minwidth=0, width=80, stretch=NO, anchor=CENTER)
+        self.ExListView.column("Category", minwidth=0, width=120, stretch=NO)
+        self.ExListView.column("Description", minwidth=0, width=250)
+        self.ExListView.column("Amount Spent", minwidth=0, width=120, stretch=NO, anchor=CENTER)
+        self.ExListView.column("Paid Through", minwidth=0, width=90, stretch=NO)
+
+
+        for i in ExList:
+            self.ExListView.heading(i, text=i.title())
+        
+        
+        Lb2 = Label(self.F3, text='Income Sheet:', font=(None, 15))
+        Lb2.place(x = 5, y = 320)
+
+        InList = ['Date', 'Source', 'Description', 'Income']
+        self.InListView = ttk.Treeview(
+            self.F3, columns=InList, show='headings', height=3)
+        self.InListView.place(x= 10, y= 365)
+        self.InListView.column("Date", minwidth=0, width=80, stretch=NO, anchor=CENTER)
+        self.InListView.column("Source", minwidth=0, width=120, stretch=NO)
+        self.InListView.column("Description", minwidth=0, width=250)
+        self.InListView.column("Income", minwidth=0, width=120, stretch=NO, anchor=CENTER)
+
+        for i in InList:
+            self.InListView.heading(i, text=i.title())
+
+
+        self.updateView()   
         self.root.mainloop()
 
+        # schedule.every().day.at("05:27").do(ut.send_notification)
+        # while True:
+        #     schedule.run_pending()
+        #     time.sleep(1)
 
-# Home.extractData("mahendra123")
+
 Home("mahendra123")
--- utilities.py
+++ utilities.py
@@ -1,8 +1,79 @@
 from datetime import date, datetime
+import mysql.connector as mysql
+import time
+from plyer import notification
+
+
+def send_notification():
+    notification.notify(
+    title = "It's time to update your expenses!!!",
+    timeout = 20
+    )
+
 
 def dateFormat(date):
-    new_date = date[:4] + "20" + date[4:]
-    new_date = datetime.strptime(new_date, "%d/%m/%Y")
+    cnt = 0
+    j = 0
+    for i in date:
+        j+=1
+        if i=='/':
+            cnt+=1
+            if cnt==2:
+                new_date = date[:j] + "20" + date[j:]
+    new_date = datetime.strptime(new_date, "%m/%d/%Y")
     new_date = datetime.date(new_date)
     new_date = str(new_date)
     return new_date
+
+
+def extractData(username, ie):
+        new_list = []
+        con = mysql.connect(host='localhost', user='root',
+                                        password='Mahendra@$28', database='expensetracker')
+        cursor = con.cursor()
+        if(ie == 'e'):
+            cursor.execute("select* from expenses where username = '"+username+"' order by date DESC limit 7")
+            edata = cursor.fetchall()
+            cursor.execute("commit")
+            for i in edata:
+                i = list(i)
+                i[0] = str(i[0])
+                i[3] = int(i[3])
+                new_list.append(i[0:len(i)-1])
+        elif(ie == 'i'):
+            cursor.execute("select* from incomes where username = '"+username+"' order by date DESC limit 5")
+            idata = cursor.fetchall()
+            cursor.execute("commit")
+            for i in idata:
+                i = list(i)
+                i[0] = str(i[0])
+                i[3] = int(i[3])
+                new_list.append(i[0:len(i)-1])
+        con.close()
+        return new_list
+
+def analysisData(username, ie, dates):
+        new_list = []
+        con = mysql.connect(host='localhost', user='root',
+                                        password='Mahendra@$28', database='expensetracker')
+        cursor = con.cursor()
+        if(ie == 'e'):
+            cursor.execute("select* from expenses where username = '"+username+"' and date BETWEEN '"+dates[0]+"' and '"+dates[1]+"' order by date ASC")
+            edata = cursor.fetchall()
+            cursor.execute("commit")
+            for i in edata:
+                i = list(i)
+                i[0] = str(i[0])
+                i[3] = int(i[3])
+                new_list.append(i[0:len(i)-1])
+        elif(ie == 'i'):
+            cursor.execute("select* from incomes where username = '"+username+"' and date BETWEEN '"+dates[0]+"' and '"+dates[1]+"' order by date ASC")
+            idata = cursor.fetchall()
+            cursor.execute("commit")
+            for i in idata:
+                i = list(i)
+                i[0] = str(i[0])
+                i[3] = int(i[3])
+                new_list.append(i[0:len(i)-1])
+        con.close()
+        return new_list
